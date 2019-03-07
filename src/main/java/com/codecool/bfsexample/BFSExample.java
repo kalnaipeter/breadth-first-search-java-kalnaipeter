0e7704f56f941dffb84a0a8cfeda754302c5b8c7
cortinadampezzo <kisgans@gmail.com>
2019-03-07 11:06:29 +0100

    done

--- src/main/java/com/codecool/bfsexample/BFSExample.java
+++ src/main/java/com/codecool/bfsexample/BFSExample.java
@@ -1,11 +1,12 @@
 package com.codecool.bfsexample;
 
+import com.codecool.bfsexample.model.BreadthFirstSearch;
 import com.codecool.bfsexample.model.UserNode;
 import java.util.List;
 
 public class BFSExample {
 
-    private static void populateDB() {
+    private static List<UserNode> populateDB() {
 
         RandomDataGenerator generator = new RandomDataGenerator();
         List<UserNode> users = generator.generate();
@@ -13,9 +14,17 @@ public class BFSExample {
         GraphPlotter graphPlotter = new GraphPlotter(users);
         
         System.out.println("Done!");
+        return users;
     }
 
     public static void main(String[] args) {
-        populateDB();
+        List<UserNode> users = populateDB();
+        UserNode user1 = users.get(34);
+        UserNode user2 = users.get(75);
+
+        BreadthFirstSearch breadthFirstSearch = new BreadthFirstSearch(users);
+        System.out.println(breadthFirstSearch.getDistanceBetweenNodes(user1, user2));
+        System.out.println(breadthFirstSearch.getFriendsOfFriends(user1, 2));
+        System.out.println(breadthFirstSearch.shortestPath(user1, user2));
     }
 }
